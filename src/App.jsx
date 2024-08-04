1c9f8b8829e702185703668d7871d35e498d98c9
Tejas More <tejasmore660@gmail.com>
2024-08-04 19:49:41 +0530

    created structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Cards from './component/Cards'
+import Form from './component/Form'
 
 function App() {
+  let [users,setUsers] = useState([])
   return (
     <>
-      <h1>running</h1>
+        <div className='w-full h-screen bg-black px-8 py-3'>
+          <Cards users={users}/>
+          <Form />
+        </div>
     </>
   )
 }
